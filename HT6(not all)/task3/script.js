698f2856676350c5257b3ea03c212e0bf8b439d0
Bogdan <bbs30042003@gmail.com>
2021-12-22 17:43:22 +0300

    HT

--- HT6(not all)/task3/script.js
+++ HT6(not all)/task3/script.js
@@ -1,12 +1,13 @@
 'use strict';
 const formBtnClick = (e) => {
-    let chBox = document.querySelectorAll('input[type=checkbox]');
-    for (let i = 0; i < chBox.length; i++) {
-        if (chBox.checked) {
-            console.log(chBox[i].value);
-        }
-        e.preventDefault();
-    }
-}
+    let formElements = e.target.elements;
+    console.log('Имя:', formElements[0].value);
+    for (let i = 0; i < formElements.length; i++) {
+        if (formElements[i].type === 'checkbox' && formElements[i].checked === true) {
+            console.log('Checkbox cheked:', formElements[i].value,);
+        };
+    };
+    e.preventDefault();
+};
 
 document.forms[0].addEventListener('submit', formBtnClick);
