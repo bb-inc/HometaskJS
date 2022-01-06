a291f62630e965f47d01ac1ea8b07804c463cf23
Bogdan <bbs30042003@gmail.com>
2022-01-06 14:03:03 +0300

    HT

---
+++ HT8/Forms/Task 1/script.js
@@ -0,0 +1,30 @@
+'use strict';
+let balanceSum = [0];
+let incomeSum = [0];
+let consSum = [0];
+const onBtn = (e) => {
+    let columnIncome = document.getElementById('columnIncome');
+    let columnCons = document.getElementById('columnCons');
+    let balance = document.getElementById('balance');
+    let formElem = e.target.elements;
+    let moneyVal = formElem['moneyVal'].value;
+    if (formElem[1].checked == true) {
+        balanceSum.push(Number(moneyVal));
+        incomeSum.push(Number(moneyVal));
+    }
+    if (formElem[2].checked == true) {
+        balanceSum.push(Number(-moneyVal));
+        consSum.push(Number(moneyVal));
+    }
+    let income = incomeSum.reduce((sum, item) => sum += item);
+    let cons = consSum.reduce((sum, item) => sum += item);
+    let bal = balanceSum.reduce((sum, item) => sum += item);
+    balance.innerText = 'Баланс:' + bal + '$';
+    columnIncome.style.height = income + 'px';
+    columnIncome.innerText = 'Доход: ' + income + '$'
+    columnCons.style.height = cons + 'px';
+    columnCons.innerText = 'Расход: ' + cons + '$';
+    e.target.reset();
+    e.preventDefault();
+}
+document.getElementById('formMoney').addEventListener('submit', onBtn);
