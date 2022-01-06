d1f19700f477f99dbc18c116dd0ac9291f02590f
Bogdan <bbs30042003@gmail.com>
2022-01-06 15:43:04 +0300

    HT

--- HT8/Forms/Task 1/script.js
+++ HT8/Forms/Task 1/script.js
@@ -16,15 +16,33 @@ const onBtn = (e) => {
         balanceSum.push(Number(-moneyVal));
         consSum.push(Number(moneyVal));
     }
-    let income = incomeSum.reduce((sum, item) => sum += item);
-    let cons = consSum.reduce((sum, item) => sum += item);
-    let bal = balanceSum.reduce((sum, item) => sum += item);
-    balance.innerText = 'Баланс:' + bal + '$';
-    columnIncome.style.height = income + 'px';
-    columnIncome.innerText = 'Доход: ' + income + '$'
-    columnCons.style.height = cons + 'px';
-    columnCons.innerText = 'Расход: ' + cons + '$';
+    if (formElem[0].value == '') {
+        alert('Вы ничего не ввели!')
+    } else {
+        let income = incomeSum.reduce((sum, item) => sum += item);
+        let cons = consSum.reduce((sum, item) => sum += item);
+        let bal = balanceSum.reduce((sum, item) => sum += item);
+        balance.innerText = 'Баланс:' + bal + '$';
+        columnIncome.style.height = income + 'px';
+        columnIncome.innerText = 'Доход: ' + income + '$'
+        columnCons.style.height = cons + 'px';
+        columnCons.innerText = 'Расход: ' + cons + '$';
+    }
     e.target.reset();
     e.preventDefault();
 }
 document.getElementById('formMoney').addEventListener('submit', onBtn);
+
+// Сделал по приколу кнопку удаления блоков
+// const onBtnDel = () => {
+//     let columnIncome = document.getElementById('columnIncome');
+//     let columnCons = document.getElementById('columnCons');
+//     let balance = document.getElementById('balance');
+//     columnIncome.style.height = 0 + 'px';
+//     columnCons.style.height = 0 + 'px';
+//     columnCons.innerText = '';
+//     columnIncome.innerText = '';
+//     balance.innerText = '';
+//     e.preventDefault();
+// }
+// document.getElementById('btnDel').addEventListener('click', onBtnDel);
